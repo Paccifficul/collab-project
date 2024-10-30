497932b7431928d51a4fae9f41d1aab94ace34ba
dmitry <elizarov.dm1337@gmail.com>
2024-10-30 23:26:20 +0300

    first commit

---
+++ src/main/java/collab/collabproject/CollabProjectApplication.java
@@ -0,0 +1,13 @@
+package collab.collabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/collab/collabproject/CollabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package collab.collabproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
