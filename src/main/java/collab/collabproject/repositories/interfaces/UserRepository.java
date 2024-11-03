2a87d092af84c7eeaae225b2df370e0acdc3eead
John Doe <johndoe@example.com>
2024-11-03 13:31:08 +0300

    to_mac

--- src/main/java/collab/collabproject/repositories/interfaces/UserRepository.java
+++ src/main/java/collab/collabproject/repositories/interfaces/UserRepository.java
@@ -1,14 +1,15 @@
 package collab.collabproject.repositories.interfaces;
 
 import collab.collabproject.models.User;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
     Optional<User> getUserById(int id);
-
     List<User> getUsers();
-
     Optional<User> addUser(String username, String email, String password);
+    Optional<User> updateUser(int id, String username, String email);
+    ResponseEntity<?> deleteUserById(int id);
 }
